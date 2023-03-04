bae4585c4f1f297ee6d8306ba8cec0175311f349
Dan Jackson <danjacksonis@googlemail.com>
2023-03-04 19:04:13 +0100

    Tidied up a bit to reduce line lengths.

--- 27-02-23-working-with-data/report_hard.py
+++ 27-02-23-working-with-data/report_hard.py
@@ -6,9 +6,12 @@ import movie_data
 from utils import pivot, pivot_ex, per_actor, average_rating, get_year
 
 def main():
-    print(f'Actor appearing in most movies: {get_popular_actor(movie_data.movies)}')
-    print(f'Actor with best average rating: {get_actor_with_best_average_rating(movie_data.movies)}')
-    print(f'Least popular genre in the 1980s: {get_least_popular_genre_in_1980s(movie_data.movies)}')
+    popular_actor = get_popular_actor(movie_data.movies)
+    print(f'Actor appearing in most movies: {popular_actor}')
+    best_actor = get_actor_with_best_average_rating(movie_data.movies)
+    print(f'Actor with best average rating: {best_actor}')
+    least_popular_genre = get_least_popular_genre_in_1980s(movie_data.movies)
+    print(f'Least popular genre in the 1980s: {least_popular_genre}')
 
 def get_popular_actor(movies):
     by_actor = pivot_ex(movies, per_actor)
@@ -21,8 +24,11 @@ def get_actor_with_best_average_rating(movies):
 
 def get_least_popular_genre_in_1980s(movies):
     by_genre = pivot(movies, 'genre')
-    by_genre = {k: [x for x in v if 1979 < get_year(x) < 1990]  for k, v in by_genre.items()}
+    by_genre = {k: movies_by_date(v, 1979, 1990) for k, v in by_genre.items()}
     return min(by_genre, key=lambda x: len(by_genre[x]))
+
+def movies_by_date(movies, start, end):
+    return [x for x in movies if start < get_year(x) < end]
 
 if __name__ == '__main__':
     main()
